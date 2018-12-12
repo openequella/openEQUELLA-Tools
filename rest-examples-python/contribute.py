9c334ce0a74f5b9d6a07e8a24ab42624868259c9
C Beach <cbeach@unicon.net>
2018-12-12 13:53:14 -0600

    #13 Initial commit of Python sample REST API code

---
+++ rest-examples-python/contribute.py
@@ -0,0 +1,59 @@
+#
+# Copyright 2017 Apereo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import equellarest
+import util
+import settings
+import uuid
+import os, stat
+
+# Create a new REST Helper which is a light wrapper around our REST api
+# Requires an Oauth Client to be set up in EQUELLA using Client Credentials flow
+equella = equellarest.EquellaRest(settings.INSTITUTION_URL, settings.OAUTH_CLIENT_ID, settings.OAUTH_CLIENT_SECRET, settings.PROXY_URL)
+
+path = os.path.dirname(os.path.realpath(__file__))
+# Ensure there is an image with the following name in this directory
+localFileName = 'myimage.jpg'
+localFilePath = os.path.join(path, localFileName)
+fileSize = os.lstat ( localFilePath )[stat.ST_SIZE]
+
+name = settings.contribute_settings['ITEM_NAME']
+description = settings.contribute_settings['ITEM_DESCRIPTION']
+
+# You cannot provide name and/or description except in the metadata.  
+# The location of the name and description nodes will depend on your schema.					
+item = {'metadata': settings.contribute_settings['ITEM_METADATA_TEMPLATE'] % (name, description, 'uuid:attach') }			
+item['collection'] = { 'uuid' : settings.contribute_settings['COLLECTION_UUID'] }
+item['attachments'] = [{	"type": "file",
+					"uuid": "uuid:attach",
+					"description": localFileName,
+					"filename": localFileName
+				}]
+
+
+# Create a staging area that we can upload files to
+staging = equella.createStaging()
+print staging
+
+# Upload the file to the staging area
+localAttachmentFile = open (localFilePath, 'rb')
+json = equella.uploadFile(staging['uuid'],  localFileName, localAttachmentFile, fileSize)
+localAttachmentFile.close()
+print json
+
+# Create a new live EQUELLA item from our object then print it out
+item = equella.newItem(item, draft = 'false', staging = staging['uuid'])
+print item
---
+++ rest-examples-python/util.py
@@ -0,0 +1,41 @@
+#
+# Copyright 2017 Apereo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import time
+import urllib
+import binascii
+import hashlib
+
+
+
+def urlEncode(text):
+	return urllib.urlencode ({'q': text}) [2:]
+
+# Generate an equella token for logging in
+def generateToken(username, sharedSecretId, sharedSecretValue):
+	seed = str (int (time.time ())) + '000'
+	id2 = urlEncode (sharedSecretId)
+	if(not(sharedSecretId == '')):
+		id2 += ':'
+
+	return '%s:%s%s:%s' % (
+			urlEncode(username),
+			id2,
+			seed,
+			binascii.b2a_base64(hashlib.md5(
+				username + sharedSecretId + seed + sharedSecretValue
+			).digest())
+		)
